0fdb05d4ab53c72956096fcfa1280767b0d6af51
Maxim Gogitidze <cocsqudriceps@yandex.ru>
2021-03-15 06:04:05 +0300

    fix: delete ternary operator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 exports.min = function min(array) {
     if (array === undefined || array.length === 0)
         return 0
-    return array.reduce((acc, cur) => acc < cur ? acc : cur)
+    return array.reduce((acc, cur) => {
+        if (acc < cur)
+            return acc
+        else
+            return cur
+    })
 }
 
 exports.max = function max(array) {
     if (array === undefined || array.length === 0)
         return 0
-    return array.reduce((acc, cur) => acc > cur ? acc : cur) ?? 0
+    return array.reduce((acc, cur) => {
+        if (acc > cur)
+            return acc
+        else
+            return cur
+    })
 }
 
 exports.avg = function avg(array) {
